5fc473914d3af0c7ac8cd2379d4decc5b308fb2e
Javed Ahmad <ja8688126@gmail.com>
2026-02-16 00:13:55 +0530

    Disable Firebase configuration in firebase.config.js
    Comment out Firebase admin initialization and service account loading.

--- Backend/modules/notification/config/firebase.config.js
+++ Backend/modules/notification/config/firebase.config.js
@@ -7,16 +7,16 @@
 
 // export default admin;
 
-import admin from "firebase-admin";
-import { readFileSync } from "fs";
+// import admin from "firebase-admin";
+// import { readFileSync } from "fs";
 
-const serviceAccount = JSON.parse(
-  readFileSync(new URL("./firebaseServiceAccount.json", import.meta.url))
-);
+// const serviceAccount = JSON.parse(
+//   readFileSync(new URL("./firebaseServiceAccount.json", import.meta.url))
+// );
 
-export const firebaseApp = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// export const firebaseApp = admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
-export default admin;
+// export default admin;
 
